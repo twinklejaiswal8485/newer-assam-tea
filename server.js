aa500c4c35ba4b1ff1be6d2cdaadb288ecd7a898
Kartik Rai <twinklejaiswal8485@gmail.com>
2026-05-13 14:51:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.listen(3000, () => console.log('ASSAM TEA running at http://localhost:3000'));
